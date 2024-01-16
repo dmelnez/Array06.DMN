2596c1ff8e08b7beaaea5507a28b52bbdcb5452a
David Melendez <145343465+dmelnez@users.noreply.github.com>
2024-01-16 18:53:03 +0100

    Add files via upload

---
+++ Array06DMN/Array06DMN/Dtos/ClienteDtos.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array06DMN.Dtos
+{
+    /// <summary>
+    /// Clase que contendra todos los atributos que compondran a un cliente
+    /// <author>DMN - 16/01/2024</author>
+    /// </summary>
+    internal class ClienteDtos
+    {
+
+        long idCliente;
+        string nombreCliente = "aaaaaa";
+        string apellidosCliente = "aaaaaa";
+        string nombreCompletoCliente = "aaaaaa";
+        int edad = 111111;
+
+
+
+        public long IdCliente { get => idCliente; set => idCliente = value; }
+        public string NombreCliente { get => nombreCliente; set => nombreCliente = value; }
+        public string ApellidosCliente { get => apellidosCliente; set => apellidosCliente = value; }
+        public string NombreCompletoCliente { get => nombreCompletoCliente; set => nombreCompletoCliente = value; }
+        public int Edad { get => edad; set => edad = value; }
+
+
+
+        public ClienteDtos(long idCliente, string nombreCliente, string apellidosCliente, string nombreCompletoCliente, int edad)
+        {
+            this.idCliente = idCliente;
+            this.nombreCliente = nombreCliente;
+            this.apellidosCliente = apellidosCliente;
+            this.nombreCompletoCliente = nombreCompletoCliente;
+            this.edad = edad;
+           
+        }
+
+
+
+        public ClienteDtos() { }
+
+
+        override
+
+        public string ToString()
+        {
+
+
+            string todo =
+                "Nombre: " + this.nombreCliente + this.apellidosCliente +
+                " | Edad Cliente: " + this.edad;
+
+            return todo;
+
+
+
+        }
+
+
+
+    }
+}
